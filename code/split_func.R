95b6abc164745d44e6363402a54f562b132fe3e4
Jesse Schneider <jsa378@student.ubc.ca>
2024-05-11 17:24:15 -0700

    which region split

--- code/split_func.R
+++ code/split_func.R
@@ -1,15 +1,20 @@
 split_func = function(region, chosen_points, test_func_vals) {
   # this function has to take in one region, and return two regions, right?
   # I probably need to check whether a proposed subregion has no points
+  # although maybe the step where we add new points
+  # to the region chosen to be split will take care of this
   dim = length(region)
-  indices_of_points_in_region = which(
-    chosen_points[, 1] >= region[[1]][[1]] &
-    chosen_points[, 1] <= region[[1]][[2]] &
-    chosen_points[, 2] >= region[[2]][[1]] &
-    chosen_points[, 2] <= region[[2]][[2]]
-  )
-  chosen_points_in_region = chosen_points[indices_of_points_in_region, ]
-  test_func_vals_in_region = test_func_vals[indices_of_points_in_region]
+  # indices_of_points_in_region = which(
+  #   chosen_points[, 1] >= region[[1]][[1]] &
+  #   chosen_points[, 1] <= region[[1]][[2]] &
+  #   chosen_points[, 2] >= region[[2]][[1]] &
+  #   chosen_points[, 2] <= region[[2]][[2]]
+  # )
+  # chosen_points_in_region = chosen_points[indices_of_points_in_region, ]
+  # test_func_vals_in_region = test_func_vals[indices_of_points_in_region]
+  points_in_region = filter_points_region_func(region, chosen_points, test_func_vals)
+  chosen_points_in_region = points_in_region[[1]]
+  test_func_vals_in_region = points_in_region[[2]]
   first_split_midpoint = mean(region[[1]])
   first_split_region_1 = list(
     c(region[[1]][[1]], first_split_midpoint),
@@ -28,41 +33,53 @@ split_func = function(region, chosen_points, test_func_vals) {
     region[[1]],
     c(second_split_midpoint, region[[2]][[2]])
   )
-  indices_of_points_in_first_split_region_1 = which(
-    chosen_points_in_region[, 1] >= first_split_region_1[[1]][[1]] &
-    chosen_points_in_region[, 1] <= first_split_region_1[[1]][[2]] &
-    chosen_points_in_region[, 2] >= first_split_region_1[[2]][[1]] &
-    chosen_points_in_region[, 2] <= first_split_region_1[[2]][[2]]
-  )
-  chosen_points_in_first_split_region_1 = chosen_points_in_region[indices_of_points_in_first_split_region_1, ]
-  test_func_vals_in_first_split_region_1 = test_func_vals_in_region[indices_of_points_in_first_split_region_1]
+  # indices_of_points_in_first_split_region_1 = which(
+  #   chosen_points_in_region[, 1] >= first_split_region_1[[1]][[1]] &
+  #   chosen_points_in_region[, 1] <= first_split_region_1[[1]][[2]] &
+  #   chosen_points_in_region[, 2] >= first_split_region_1[[2]][[1]] &
+  #   chosen_points_in_region[, 2] <= first_split_region_1[[2]][[2]]
+  # )
+  # chosen_points_in_first_split_region_1 = chosen_points_in_region[indices_of_points_in_first_split_region_1, ]
+  # test_func_vals_in_first_split_region_1 = test_func_vals_in_region[indices_of_points_in_first_split_region_1]
+  points_in_first_split_region_1 = filter_points_region_func(first_split_region_1, chosen_points_in_region, test_func_vals_in_region)
+  chosen_points_in_first_split_region_1 = points_in_first_split_region_1[[1]]
+  test_func_vals_in_first_split_region_1 = points_in_first_split_region_1[[2]]
   test_func_avg_vals_in_first_split_region_1 = mean(test_func_vals_in_first_split_region_1)
-  indices_of_points_in_first_split_region_2 = which(
-    chosen_points_in_region[, 1] >= first_split_region_2[[1]][[1]] &
-    chosen_points_in_region[, 1] <= first_split_region_2[[1]][[2]] &
-    chosen_points_in_region[, 2] >= first_split_region_2[[2]][[1]] &
-    chosen_points_in_region[, 2] <= first_split_region_2[[2]][[2]]
-  )
-  chosen_points_in_first_split_region_2 = chosen_points_in_region[indices_of_points_in_first_split_region_2, ]
-  test_func_vals_in_first_split_region_2 = test_func_vals_in_region[indices_of_points_in_first_split_region_2]
+  # indices_of_points_in_first_split_region_2 = which(
+  #   chosen_points_in_region[, 1] >= first_split_region_2[[1]][[1]] &
+  #   chosen_points_in_region[, 1] <= first_split_region_2[[1]][[2]] &
+  #   chosen_points_in_region[, 2] >= first_split_region_2[[2]][[1]] &
+  #   chosen_points_in_region[, 2] <= first_split_region_2[[2]][[2]]
+  # )
+  # chosen_points_in_first_split_region_2 = chosen_points_in_region[indices_of_points_in_first_split_region_2, ]
+  # test_func_vals_in_first_split_region_2 = test_func_vals_in_region[indices_of_points_in_first_split_region_2]
+  points_in_first_split_region_2 = filter_points_region_func(first_split_region_2, chosen_points_in_region, test_func_vals_in_region)
+  chosen_points_in_first_split_region_2 = points_in_first_split_region_2[[1]]
+  test_func_vals_in_first_split_region_2 = points_in_first_split_region_2[[2]]
   test_func_avg_vals_in_first_split_region_2 = mean(test_func_vals_in_first_split_region_2)
-  indices_of_points_in_second_split_region_1 = which(
-    chosen_points_in_region[, 1] >= second_split_region_1[[1]][[1]] &
-    chosen_points_in_region[, 1] <= second_split_region_1[[1]][[2]] &
-    chosen_points_in_region[, 2] >= second_split_region_1[[2]][[1]] &
-    chosen_points_in_region[, 2] <= second_split_region_1[[2]][[2]]
-  )
-  chosen_points_in_second_split_region_1 = chosen_points_in_region[indices_of_points_in_second_split_region_1, ]
-  test_func_vals_in_second_split_region_1 = test_func_vals_in_region[indices_of_points_in_second_split_region_1]
+  # indices_of_points_in_second_split_region_1 = which(
+  #   chosen_points_in_region[, 1] >= second_split_region_1[[1]][[1]] &
+  #   chosen_points_in_region[, 1] <= second_split_region_1[[1]][[2]] &
+  #   chosen_points_in_region[, 2] >= second_split_region_1[[2]][[1]] &
+  #   chosen_points_in_region[, 2] <= second_split_region_1[[2]][[2]]
+  # )
+  # chosen_points_in_second_split_region_1 = chosen_points_in_region[indices_of_points_in_second_split_region_1, ]
+  # test_func_vals_in_second_split_region_1 = test_func_vals_in_region[indices_of_points_in_second_split_region_1]
+  points_in_second_split_region_1 = filter_points_region_func(second_split_region_1, chosen_points_in_region, test_func_vals_in_region)
+  chosen_points_in_second_split_region_1 = points_in_second_split_region_1[[1]]
+  test_func_vals_in_second_split_region_1 = points_in_second_split_region_1[[2]]
   test_func_avg_vals_in_second_split_region_1 = mean(test_func_vals_in_second_split_region_1)
-  indices_of_points_in_second_split_region_2 = which(
-    chosen_points_in_region[, 1] >= second_split_region_2[[1]][[1]] &
-      chosen_points_in_region[, 1] <= second_split_region_2[[1]][[2]] &
-      chosen_points_in_region[, 2] >= second_split_region_2[[2]][[1]] &
-      chosen_points_in_region[, 2] <= second_split_region_2[[2]][[2]]
-  )
-  chosen_points_in_second_split_region_2 = chosen_points_in_region[indices_of_points_in_second_split_region_2, ]
-  test_func_vals_in_second_split_region_2 = test_func_vals_in_region[indices_of_points_in_second_split_region_2]
+  # indices_of_points_in_second_split_region_2 = which(
+  #   chosen_points_in_region[, 1] >= second_split_region_2[[1]][[1]] &
+  #     chosen_points_in_region[, 1] <= second_split_region_2[[1]][[2]] &
+  #     chosen_points_in_region[, 2] >= second_split_region_2[[2]][[1]] &
+  #     chosen_points_in_region[, 2] <= second_split_region_2[[2]][[2]]
+  # )
+  # chosen_points_in_second_split_region_2 = chosen_points_in_region[indices_of_points_in_second_split_region_2, ]
+  # test_func_vals_in_second_split_region_2 = test_func_vals_in_region[indices_of_points_in_second_split_region_2]
+  points_in_second_split_region_2 = filter_points_region_func(second_split_region_2, chosen_points_in_region, test_func_vals_in_region)
+  chosen_points_in_second_split_region_2 = points_in_second_split_region_2[[1]]
+  test_func_vals_in_second_split_region_2 = points_in_second_split_region_2[[2]]
   test_func_avg_vals_in_second_split_region_2 = mean(test_func_vals_in_second_split_region_2)
   test_func_avg_vals = c(
     test_func_avg_vals_in_first_split_region_1, 
@@ -70,7 +87,7 @@ split_func = function(region, chosen_points, test_func_vals) {
     test_func_avg_vals_in_second_split_region_1,
     test_func_avg_vals_in_second_split_region_2
   )
-  if (which.max(test_func_avg_vals) <= 2) {
+  if (which.min(test_func_avg_vals) <= 2) {
     return(list(first_split_region_1, first_split_region_2))
   } else {
     return(list(second_split_region_1, second_split_region_2))
--- notes.md
+++ notes.md
@@ -14,16 +14,16 @@ To install the xmengju/EGO R package from [https://github.com/xmengju/EGO](https
 
 The rough idea is to try to adapt the ADAPT algorithm for use in Bayesian Optimization.
 
-1. Generate points in $X = \mathcal R_1$, the domain of $f$
+1. Generate points in $X$, the domain of $f$
 2. Evaluate $f$ at the points generated
 3. Train GP on those chosen points and observations
-4. Optimize the/an acquisition function on $\mathcal R_1$
-5. Generate more new points within $\mathcal R_1$
+4. Optimize the/an acquisition function on $X$
+5. Generate more new points within $X$
 6. Evaluate $f$ at the new points
-7. Now we decide how to split $\mathcal R_1$:
+7. Now we decide how to split $X$:
     1. Split the first axis at the midpoint
     2. For each hypothetical sub-region, compute the average of the observed values of $f$
-    3. Select the split that leads to the highest average of observed values of $f$ (of the 4 values you will get)
+    3. Select the split that leads to the *lowest* average of observed values of $f$ (of the 4 values you will get)
 8. Now we have two regions, $\mathcal R_1$ and $\mathcal R_2$
 9. Fit a new GP model for each of $\mathcal R_1$ and $\mathcal R_2$
 9. Select either Method 1 or Method 2 (below) to decide which of $\mathcal R_1$ or $\mathcal R_2$ to split
@@ -32,13 +32,15 @@ The rough idea is to try to adapt the ADAPT algorithm for use in Bayesian Optimi
 
 1. Optimize an acquisition function in each of $\mathcal R_1$ and $\mathcal R_2$, resulting in proposed new sampling points $x_1^*$ and $x_2^*$
 2. Evaluate $f$ at each of $x_1^*$ and $x_2^*$
-3. Select the region that saw the highest $f$ value for splitting
+3. Select for splitting the region that saw the lowest $f$ value
 
 I think with this method, if we have three regions $\mathcal R_1, \mathcal R_2$ and $\mathcal R_3$, then if we decided to split $\mathcal R_2$ into $\mathcal R_2$ and $\mathcal R_3$, when deciding which of the three regions to split, we don't have to optimize the acquisition function for $\mathcal R_1$ again, because nothing has changed in that region.
 
+Note: For this method to be honest, I would probably need to count the evaluations $f(x_1^*)$ and $f(x_2^*)$ towards the total budget of $N$ observations.
+
 ## Method 2
 
-1. Optimize EI in each of $\mathcal R_1$ and $\mathcal R_2$, *using* $\boldsymbol f^*$, *the highest observed value in any region*
-2. Choose for splitting the region that returns the highest of the EI values from the previous step
+1. Optimize EI in each of $\mathcal R_1$ and $\mathcal R_2$, *using* $\boldsymbol f^*$, the *lowest* observed value in any region
+2. Choose for splitting the region that returns the *highest* of the EI values from the previous step
 
-I think with this method, we have to re-optimize EI for every region, if in the last round, we observed a new highest observed max $\boldsymbol f^*$, right?
+I think with this method, we have to re-optimize EI for every region, if in the last round, we observed a new lowest observed min $\boldsymbol f^*$, right?
