0444f1ce6914f5fdab32f6ce91dbda708bd5bf36
Jesse Schneider <jsa378@student.ubc.ca>
2024-06-14 19:16:00 -0700

    gen init points

---
+++ code/implementation_testing/init_points/gen_init_points.R
@@ -0,0 +1,45 @@
+library(EGOmod2)
+
+args = commandArgs(trailingOnly = TRUE)
+if (length(args) < 5){
+  stop("Five arguments must be supplied: test function (string), dim (int), num init obs (int), num runs (int) and save dir (no type)", call. = FALSE)
+}
+
+test_func_name = args[1]
+dim = as.integer(args[2])
+num_init_obs = as.integer(args[3])
+num_runs = as.integer(args[4])
+save_dir = as.character(args[5])
+
+source("/home/jsa378/bo_partition/code/test_funcs.R")
+
+x_names_arg = character(0)
+for (d in 1:dim){
+  x_names_arg = c(x_names_arg, sprintf("x%s", d))
+}
+
+test_func = test_func_list[[test_func_name]]$func
+test_lbound = test_func_list[[test_func_name]]$lbound
+test_ubound = test_func_list[[test_func_name]]$ubound
+
+descr = DescribeX(
+  x_names = x_names_arg,
+  x_min = test_lbound,
+  x_max = test_ubound,
+  support = rep("Continuous", dim)
+)
+
+for(run in 1:num_runs){
+  init = Initialize(
+    n_design = num_init_obs,
+    x_describe = descr,
+    fun = test_func,
+    n_rep = 0
+  )
+  
+  write.table(init$x_design,
+              file = sprintf("%s%s_%s_dim_run_%s_init_points.csv", save_dir, test_func_name, dim, run),
+              row.names = FALSE,
+              col.names = FALSE
+  )
+}
---
+++ code/implementation_testing/init_points/gen_init_points.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --account=def-wjwelch    # replace this with your own account
+#SBATCH --mem-per-cpu=1000M      # memory; default unit is megabytes
+#SBATCH --array=1-1              # number of array jobs, inclusive
+#SBATCH --time=0-00:10           # time (DD-HH:MM)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jsa378@sfu.ca
+#SBATCH --output=name%j.out
+
+module purge
+module load StdEnv/2023 gcc/12.3 r/4.4.0     # Adjust version and add the gcc module used for installing packages.
+
+TEST_FUNC="rastr"
+DIM=10
+NUM_INIT_OBS=40
+NUM_RUNS=10
+SAVE_DIR=/home/jsa378/bo_partition/code/implementation_testing/init_points/${TEST_FUNC}_${DIM}_dim/
+
+printf "Generating $NUM_INIT_OBS init points for $NUM_RUNS for the $TEST_FUNC test function."
+printf "The init points will be saved in $SAVE_DIR."
+
+if [ ! -d "$SAVE_DIR" ]
+then
+  echo "The save directory $SAVE_DIR does not exist, so we will create it."
+  mkdir $SAVE_DIR
+else
+  echo "The save directory $SAVE_DIR exists, so we don't need to create it."
+fi
+
+Rscript /home/jsa378/bo_partition/code/implementation_testing/init_points/gen_init_points.R $TEST_FUNC $DIM $NUM_INIT_OBS $NUM_RUNS $SAVE_DIR
+
+printf "Init points created and saved."
