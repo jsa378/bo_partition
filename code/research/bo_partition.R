20eb1729caf287e6a4620880c83979d5d02658a2
Jesse Schneider <jsa378@student.ubc.ca>
2024-06-20 16:00:42 -0700

    bo_partition

--- code/research/bo_partition.R
+++ code/research/bo_partition.R
@@ -108,137 +108,53 @@ descr <- DescribeX(
 )
 init <- Initialize(
   x_design = init_points,
-  n_design = num_init_obs,
   x_describe = descr,
+  fun = test_func
+)
+init_bo <- EGO(
   fun = test_func,
-  n_rep = 0
+  reg_model = ~1,
+  ego_init = init,
+  x_describe = descr,
+  nsteps = 1,
+  control = ctrl
 )
 
-smallest_y_so_far = min(init$y_design)
-entire_region = list(bound_matrix = as.matrix(cbind(test_lbound, test_ubound)),
-                     region_x = init$x_design,
-                     region_y = init$y_design,
-                     region_min = min(init$y_design),
-                     region_argmin = init$x_design[which.min(init$y_design), ]
-                     )
-all_regions = list(entire_region)
+init_region = list(bound_matrix = as.matrix(cbind(test_lbound, test_ubound)),
+                   region_x = init_bo$x,
+                   region_y = init_bo$y,
+                   region_min = min(init_bo$y),
+                   region_argmin = init_bo$x[which.min(init_bo$y), ],
+                   region_a_max = init_bo$ac_val_track
+)
+all_regions = list(init_region)
+smallest_y_so_far = min(init_bo$y)
 
 n_tot = 100
 
-split = function(region) {
-  
-  region_x = region$region_x
-  region_y = region$region_y
-  lowest_y_avg_val = 1e+10
-  
-  for(d in 1:dim){
-    med = median(region_x[, d])
-    
-    region_1_x = region_x[region_x[, d] < med, ]
-    region_1_y = region_y[which(region_x[, d] < med)]
-    region_2_x = region_x[region_x[, d] >= med, ]
-    region_2_y = region_y[which(region_x[, d] >= med)]
-    
-    region_1_y_avg = mean(region_1_y)
-    region_2_y_avg = mean(region_2_y)
-    
-    if(region_1_y_avg < lowest_y_avg_val | region_2_y_avg < lowest_y_avg_val){
-      lowest_y_avg_val = min(region_1_y_avg, region_2_y_avg)
-      dim_to_split = d
-      split_point = med
-
-      split_region_1_x = region_1_x
-      split_region_1_y = region_1_y
-      split_region_2_x = region_2_x
-      split_region_2_y = region_2_y
-    }
+while (length(all_regions) > 0) {
+  region_values = matrix(data = NA, nrow = 1, ncol = length((all_regions)))
+  for (region_index in 1:length(all_regions)){
+    current_region = all_regions[[region_index]]
+    region_values[region_index] = current_region$region_min - current_region$region_a_max
   }
-
-  region_1_return = region
-  region_1_return$bound_matrix[dim_to_split, 2] = split_point
-  region_1_return$region_x = split_region_1_x
-  region_1_return$region_y = split_region_1_y
-  region_1_return$region_min = min(split_region_1_y)
-  region_1_return$region_argmin = split_region_1_x[which.min(split_region_1_y), ]
-
-  region_2_return = region
-  region_2_return$bound_matrix[dim_to_split, 1] = split_point
-  region_2_return$region_x = split_region_2_x
-  region_2_return$region_y = split_region_2_y
-  region_2_return$region_min = min(split_region_2_y)
-  region_2_return$region_argmin = split_region_2_x[which.min(split_region_2_y), ]
-  
-  return(list(region_1_return,
-              region_2_return)
-  )
-  # the two regions this function returns
-  # need to be added to our list of candidate regions
-}
-
-explore_region <- function(region,
-                           n_max = 10,
-                           tol = 1) {
-  region_lbound = region$bound_matrix[, 1]
-  region_ubound = region$bound_matrix[, 2]
-  region_x = region$region_x
-  region_y = region$region_y
-  n = nrow(region_x)
-  descr <- DescribeX(
-    x_names = x_names_arg,
-    x_min = region_lbound,
-    x_max = region_ubound,
-    support = rep("Continuous", dim)
-  )
-  while (n <= n_max) {
-    init <- Initialize(
-      x_design = region_x,
-      x_describe = descr,
-      fun = test_func
-    )
-    bo <- EGO(
-      fun = test_func,
-      reg_model = ~1,
-      ego_init = init,
-      x_describe = descr,
-      nsteps = 1,
-      control = ctrl
-    )
-    region_x = bo$x
-    region_y = bo$y
-    n = nrow(region_x)
-    new_observed_y = tail(region_y, n = 1)
-    if (new_observed_y < region$region_min) {
-      region$region_min = new_observed_y
-      region$region_argmin = region_x[n, ]
-      if (new_observed_y < smallest_y_so_far) {
-        smallest_y_so_far = new_observed_y
-      }
-    }
-    a_max = bo$ac_val_track
-    
-    region$region_x = region_x
-    region$region_y = region_y
-    region$region_min = min(region_y)
-    region$region_argmin = which.min(region_y)
-    if (a_max < tol) {
-      # i should prepare the updated region and return it
-      # and return the smallest y value, i think
-      # and remove this region from my list of promising regions
-      # my dice_loop.R might be helpful for the last part
-
-      return(list(region,
-                  smallest_y_so_far)
-             )
-    }
+  index_of_region_to_explore <- which.min(region_values)
+  region_to_explore = all_regions[[index_of_region_to_explore]]
+  results = explore_region(region_to_explore)
+  # i think I need some way of discerning
+  # whether split_and_fit was called during explore_region or not
+  # because I may need to remove region_to_explore from all_regions
+  if (results$split_called == 0) {
+    all_regions = all_regions[-index_of_region_to_explore]
+    smallest_y_so_far = results$best_y
   }
-  # the while loop completed, so now
-  # we need to split the region into 2 subregions
-  new_subregions = split(region)
-  new_subregion_1 = new_subregions[[1]]
-  new_subregion_2 = new_subregions[[2]]
-  return(list(new_subregion_1,
-              new_subregion_2)
-         )
+  if (results$split_called == 1) {
+    all_regions = all_regions[-index_of_region_to_explore]
+    new_region_1 = results[[1]]
+    new_region_2 = results[[2]]
+    all_regions = c(all_regions, list(new_region_1, new_region_2))
+    smallest_y_so_far = results$best_y
+  }
 }
 
 end <- Sys.time()
---
+++ code/research/bo_partition_helper_funcs.R
@@ -0,0 +1,187 @@
+split_and_fit = function(region) {
+  
+  region_x = region$region_x
+  region_y = region$region_y
+  lowest_y_avg_val = 1e+10
+  
+  for(d in 1:dim){
+    med = median(region_x[, d])
+    
+    region_1_x = region_x[region_x[, d] < med, ]
+    region_1_y = region_y[which(region_x[, d] < med)]
+    region_2_x = region_x[region_x[, d] >= med, ]
+    region_2_y = region_y[which(region_x[, d] >= med)]
+    
+    region_1_y_avg = mean(region_1_y)
+    region_2_y_avg = mean(region_2_y)
+    
+    if(region_1_y_avg < lowest_y_avg_val | region_2_y_avg < lowest_y_avg_val){
+      lowest_y_avg_val = min(region_1_y_avg, region_2_y_avg)
+      dim_to_split = d
+      split_point = med
+      
+      split_region_1_x = region_1_x
+      split_region_1_y = region_1_y
+      split_region_2_x = region_2_x
+      split_region_2_y = region_2_y
+    }
+  }
+  
+  region_1_return = region
+  region_1_return$bound_matrix[dim_to_split, 2] = split_point
+  region_1_return$region_x = split_region_1_x
+  region_1_return$region_y = split_region_1_y
+  region_1_return$region_min = min(split_region_1_y)
+  region_1_return$region_argmin = split_region_1_x[which.min(split_region_1_y), ]
+  
+  region_2_return = region
+  region_2_return$bound_matrix[dim_to_split, 1] = split_point
+  region_2_return$region_x = split_region_2_x
+  region_2_return$region_y = split_region_2_y
+  region_2_return$region_min = min(split_region_2_y)
+  region_2_return$region_argmin = split_region_2_x[which.min(split_region_2_y), ]
+  
+  region_1_descr <- DescribeX(
+    x_names = x_names_arg,
+    x_min = region_1_return$bound_matrix[, 1],
+    x_max = region_1_return$bound_matrix[, 2],
+    support = rep("Continuous", dim)
+  )
+  
+  region_1_init <- Initialize(
+    x_design = region_1_return$region_x,
+    x_describe = region_1_descr,
+    fun = test_func
+  )
+  
+  region_2_descr <- DescribeX(
+    x_names = x_names_arg,
+    x_min = region_2_return$bound_matrix[, 1],
+    x_max = region_2_return$bound_matrix[, 2],
+    support = rep("Continuous", dim)
+  )
+  
+  region_2_init <- Initialize(
+    x_design = region_2_return$region_x,
+    x_describe = region_2_descr,
+    fun = test_func
+  )
+  
+  region_1_bo <- EGO(
+    fun = test_func,
+    reg_model = ~1,
+    ego_init = region_1_init,
+    x_describe = region_1_descr,
+    nsteps = 1,
+    control = ctrl
+  )
+  
+  region_2_bo <- EGO(
+    fun = test_func,
+    reg_model = ~1,
+    ego_init = region_2_init,
+    x_describe = region_2_descr,
+    nsteps = 1,
+    control = ctrl
+  )
+  
+  region_1_return$region_x <- region_1_bo$x
+  region_1_return$region_y <- region_1_bo$y
+  new_region_1_obs = tail(region_1_bo$y, n = 1)
+  if (new_region_1_obs < region_1_return$region_min) {
+    region_1_return$region_min = new_region_1_obs
+    region_1_return$region_argmin = region_1_bo$x[nrow(region_1_bo$x), ]
+    if (new_region_1_obs < smallest_y_so_far) {
+      smallest_y_so_far = new_region_1_obs
+    }
+  }
+  region_1_return$a_max <- region_1_bo$ac_val_track
+  
+  region_2_return$region_x <- region_2_bo$x
+  region_2_return$region_y <- region_2_bo$y
+  new_region_2_obs = tail(region_2_bo$y, n = 1)
+  if (new_region_2_obs < region_2_return$region_min) {
+    region_2_return$region_min = new_region_2_obs
+    region_2_return$region_argmin = region_2_bo$x[nrow(region_2_bo$x), ]
+    if (new_region_2_obs < smallest_y_so_far) {
+      smallest_y_so_far = new_region_2_obs
+    }
+  }
+  region_2_return$a_max <- region_2_bo$ac_val_track
+  
+  return(list(region_1_return,
+              region_2_return,
+              new_best_y = smallest_y_so_far
+              )
+  )
+  # the two regions this function returns
+  # need to be added to our list of candidate regions
+}
+
+explore_region <- function(region,
+                           n_max = 10,
+                           tol = 1) {
+  region_x = region$region_x
+  region_y = region$region_y
+  n = nrow(region_x)
+  descr <- DescribeX(
+    x_names = x_names_arg,
+    x_min = region$bound_matrix[, 1],
+    x_max = region$bound_matrix[, 2],
+    support = rep("Continuous", dim)
+  )
+  while (n <= n_max) {
+    init <- Initialize(
+      x_design = region_x,
+      x_describe = descr,
+      fun = test_func
+    )
+    bo <- EGO(
+      fun = test_func,
+      reg_model = ~1,
+      ego_init = init,
+      x_describe = descr,
+      nsteps = 1,
+      control = ctrl
+    )
+    region_x = bo$x
+    region_y = bo$y
+    n = nrow(region_x)
+    new_observed_y = tail(region_y, n = 1)
+    if (new_observed_y < region$region_min) {
+      region$region_min = new_observed_y
+      region$region_argmin = region_x[n, ]
+      if (new_observed_y < smallest_y_so_far) {
+        smallest_y_so_far = new_observed_y
+      }
+    }
+    a_max = bo$ac_val_track
+    
+    region$region_x = region_x
+    region$region_y = region_y
+    region$region_min = min(region_y)
+    region$region_argmin = which.min(region_y)
+    if (a_max < tol) {
+      # i should prepare the updated region and return it
+      # and return the smallest y value, i think
+      # and remove this region from my list of promising regions
+      # my dice_loop.R might be helpful for the last part
+      
+      return(list(region = region,
+                  best_y = smallest_y_so_far,
+                  split_called = 0)
+      )
+    }
+  }
+  # the while loop completed, so now
+  # we need to split the region into 2 subregions
+  new_subregions = split_and_fit(region)
+  new_subregion_1 = new_subregions[[1]]
+  new_subregion_2 = new_subregions[[2]]
+  new_best_y = new_subregions$new_best_y
+  return(list(new_region_1 = new_subregion_1,
+              new_region_2 = new_subregion_2,
+              best_y = new_best_y,
+              split_called = 1)
+  )
+}
