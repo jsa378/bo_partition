110ea755b796c4ba68d53721d411f78d8f11fe24
Jesse Schneider <jsa378@student.ubc.ca>
2024-07-07 15:22:26 -0700

    debug setup

--- code/research/debug/bo_partition.sh
+++ code/research/debug/bo_partition.sh
@@ -8,9 +8,9 @@
 
 ulimit -c unlimited
 module purge
-module load StdEnv/2023 gcc/12.3 r/4.4.0     # Adjust version and add the gcc module used for installing packages.
+module load StdEnv/2023 gcc/12.3 r/4.4.0 openmpi/4.1.5 valgrind-mpi/3.21.0    # Adjust version and add the gcc module used for installing packages.
 
 printf "The current job ID is: $SLURM_JOB_ID\n"
-mkdir /home/jsa378/scratch/ego_valgrind/
+# mkdir /home/jsa378/scratch/ego_valgrind/
 
 R -d "valgrind --tool=memcheck --leak-check=yes --show-reachable=yes" < /home/jsa378/bo_partition/code/research/debug/bo_partition.R
