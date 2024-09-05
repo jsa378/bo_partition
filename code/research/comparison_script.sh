325bfff1791c21a12c0a392decc968bafdcac5a1
Jesse Schneider <jsa378@student.ubc.ca>
2024-09-05 12:23:42 -0700

    can now run v6 w/ diff epsilons

--- code/research/comparison_script.sh
+++ code/research/comparison_script.sh
@@ -10,6 +10,7 @@ DIM=5
 NUM_INIT_OBS=50
 NUM_SUBSEQ_OBS=200
 N_MAX=100
+EPSILON=0.01 # 0.05 # 0.1 # 0.25
 
 # The reason I make the directories below
 # and then navigate into them before
@@ -36,7 +37,12 @@ N_MAX=100
 # sbatch /home/jsa378/bo_partition/code/research/v5/array.sh
 
 # Test v6
+# To run v6 with different values of epsilon,
+# comment out regular Dice and v4 and v5 above,
+# then modify EPSILON above and in v6/array.sh
+# to the same value,
+# and then submit this script.
 
-mkdir /home/jsa378/scratch/${TEST_FUNC}_v6_${DIM}_dim_${NUM_INIT_OBS}_initobs_${N_MAX}_nmax_${NUM_SUBSEQ_OBS}_subseqobs/
-cd /home/jsa378/scratch/${TEST_FUNC}_v6_${DIM}_dim_${NUM_INIT_OBS}_initobs_${N_MAX}_nmax_${NUM_SUBSEQ_OBS}_subseqobs/
+mkdir /home/jsa378/scratch/${TEST_FUNC}_v6_${DIM}_dim_${NUM_INIT_OBS}_initobs_${N_MAX}_nmax_${NUM_SUBSEQ_OBS}_subseqobs_${EPSILON}_epsilon/
+cd /home/jsa378/scratch/${TEST_FUNC}_v6_${DIM}_dim_${NUM_INIT_OBS}_initobs_${N_MAX}_nmax_${NUM_SUBSEQ_OBS}_subseqobs_${EPSILON}_epsilon/
 sbatch /home/jsa378/bo_partition/code/research/v6/array.sh
